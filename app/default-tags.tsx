79e1f161135720ba3de2e4b2e260e1fb0ab683b8
abdulroheem <ayanwunmiabdulroheem@gmail.com>
2023-08-04 19:07:15 +0100

    first commit

--- app/default-tags.tsx
+++ app/default-tags.tsx
@@ -20,18 +20,18 @@ export default function DefaultTags() {
         sizes="16x16"
         href="/favicon-16x16.png"
       />
-      <meta name="twitter:title" content="Qudusayo" />
+      <meta name="twitter:title" content="Dev_cody" />
       <meta
         name="twitter:description"
-        content="Hello there, I'm Qudusayo. I'm a full-stack developer."
+        content="Hello there, I'm Abdulroheem. I'm a full-stack developer."
       />
       <meta
         name="twitter:image:src"
         content="/android-chrome-512x512.png"
       />
-      <meta name="twitter:image:alt" content="Check what Qudusayo is doing" />
-      <meta name="twitter:creator" content="@qudusayo" />
-      <meta name="twitter:site" content="@qudusayo" />
+      <meta name="twitter:image:alt" content="Check what Dev_cody is doing" />
+      <meta name="twitter:creator" content="@abdulroheem_" />
+      <meta name="twitter:site" content="@abdulroheem_" />
     </>
   );
 }
